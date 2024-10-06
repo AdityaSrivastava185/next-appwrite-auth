89f95fa50b5404b59dc5b05dd9ced76db2831531
Aditya-Srivastava <pks85715@gmail.com>
2024-10-06 22:45:54 +0530

    login file added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,36 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Next auth 
+       <main className="container mx-auto max-w-[800px]">
+            <div id="orders-container">
+                <strong>Orders</strong>
+                <p>Recent orders from your store.</p>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Customer</th>
+                            <th>Status</th>
+                            <th>Type</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => (
+                            <tr key={order.$id}>
+                                <td className="flex flex-col">
+                                    <strong>{order.customer}</strong>
+                                    <p>{order.customer_email}</p>
+                                </td>
+                                <td>{order.status}</td>
+                                <td>{order.type}</td>
+                                <td>${order.total}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </main>
     </div>
   );
 }
